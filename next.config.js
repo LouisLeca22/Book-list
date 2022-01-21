f503912baabee53a8fa86a4e5905dd619278727b
LouisLeca22 <66520052+LouisLeca22@users.noreply.github.com>
2022-01-21 19:33:11 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,3 @@
 module.exports = {
   reactStrictMode: true,
-  env: {
-    MONGO_URI:
-      'mongodb+srv://louis123:louis123@booklist.sx2hh.mongodb.net/booklist?retryWrites=true&w=majority',
-  }
 };
